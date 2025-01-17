3f8f8c250493d79749c0a2634657b3de2c6f5a5d
Filip <mach.filip24@gmail.com>
2025-01-17 23:50:07 +0100

    Basic GUI

---
+++ Library/src/main/java/com/example/library/models/BookInstance.java
@@ -0,0 +1,11 @@
+package com.example.library.models;
+
+import javafx.beans.property.IntegerProperty;
+import java.util.UUID;
+
+public class BookInstance {
+    public UUID bookId;
+    public Book book;
+    public String publisher;
+    public int length;
+}
